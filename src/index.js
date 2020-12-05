7898cea2ceb5d53589a031335489ffbe951de146
Likai Lee <likailee.cn@gmail.com>
2020-12-05 19:30:07 +0800

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -11,27 +11,32 @@ function generateProgressBar() {
         .fill('▁')
         .map((value, index) => index < passedProgressBarIndex ? '█' : value)
         .join('')
-    return `{ ${progressBar} }`
+    return `${progressBar}`
 }
 
 const readme = `
+![](https://raw.githubusercontent.com/LikaiLee/likailee.github.io/img/20200818102030.png)
 <h2 align="center"><strong><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35px"> Hi, fellow <𝚌𝚘𝚍𝚎𝚛𝚜 /> !</strong></h2>
-<p align="center">
-  This is Likai Lee from Hangzhou, China 🇨🇳. I have experience in web depvelopment.  Passionated about technology, communities, and everything in-between 🌎.
+<p>
+  This is <strong>Likai Lee</strong> from Hangzhou, China 🇨🇳. I have experience in web depvelopment.  Passionated about technology, communities, and everything in-between 🌎.
+</p>
+
+<h2 align="center"><strong>⏳  Year progress @2020</strong></h2>
+<p align="center" style="color: orange">
+    ${progressBarOfThisYear}<b style="color: #000">${(progressOfThisYear * 100).toFixed(2)}%</b>
 </p>
 
 <h2 align="center"><strong>😜 Here's a Joke for you</strong></h2>
 <p align="center">
-  <img src="https://readme-jokes.vercel.app/api" alt="Jokes Card" />
+  <img src="https://readme-jokes.vercel.app/api?theme=vue" alt="Jokes Card" />
 </p>
-
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
 
-⏰ Updated on ${new Date().toUTCString()}
+<p align="right">⏰ Updated on Sat, 05 Dec 2020 10:39:09 GMT</p>
 
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)
+<p align="right">![Build README](https://github.com/LikaiLee/LikaiLee/workflows/Build%20README/badge.svg)</p>
+![bottom.png](https://raw.githubusercontent.com/LikaiLee/likailee.github.io/img/20200818102046.png)
 `
 
 console.log(readme)
